95d542b0b82062c61c3880793ef4b7a1eb56dcc4
supermegapupergiber <ku6636520@gmail.com>
2024-09-19 14:30:14 +0500

    Update models.py

--- models.py
+++ models.py
@@ -1,1 +1,16 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Team(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    rating = db.Column(db.Float, default=0)
+    profiles = db.relationship('Profile', backref='team', lazy=True)
+
+class Profile(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), nullable=False)
+    rating = db.Column(db.Float, default=0)
+    team_id = db.Column(db.Integer, db.ForeignKey('team.id'), nullable=True)
 
