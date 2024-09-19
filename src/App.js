c3204fa4860b4a1785ea5226e2cce5523aa5e7dc
supermegapupergiber <ku6636520@gmail.com>
2024-09-19 17:10:45 +0500

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import TeamList from './components/TeamList';
+import Profile from './components/Profile';
+
+function App() {
+  return (
+    <div>
+      <h1>Hackathon App</h1>
+      <TeamList />
+      <Profile />
+    </div>
+  );
+}
+
+export default App;
