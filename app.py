7f751f36eb209de287e983f588a96bdeba5dd324
supermegapupergiber <ku6636520@gmail.com>
2024-09-19 14:32:56 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,20 @@
+from flask import Flask
+from flask_cors import CORS
+from models import db
+from routes import main_routes
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///hackathon.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+CORS(app)
+
+with app.app_context():
+    db.create_all()
+
+app.register_blueprint(main_routes)
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
